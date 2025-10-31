288ea4b95c2ce82ff5e9ced0539a1303f651e2a2
Fidel Eliseo Reyes Ramirez <fideleliseoreyesramirez@gmail.com>
2025-10-31 18:31:32 -0400

    vamos a intentar q la sombra no corra al inicio

---
+++ Assets/Scripts/IntroController1.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+using UnityEngine.Video;
+using UnityEngine.SceneManagement;
+
+public class IntroController1 : MonoBehaviour
+{
+    [SerializeField] private VideoPlayer videoPlayer;
+    [SerializeField] private string nextSceneName = "PrimerNivel"; // escena siguiente
+
+    void Start()
+    {
+        videoPlayer.loopPointReached += OnVideoEnd;
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) || Input.GetButtonDown("Start"))
+        {
+            SkipIntro();
+        }
+    }
+
+    private void OnVideoEnd(VideoPlayer vp)
+    {
+        LoadNextScene();
+    }
+
+    private void SkipIntro()
+    {
+        videoPlayer.Stop();
+        LoadNextScene();
+    }
+
+    private void LoadNextScene()
+    {
+        SceneManager.LoadScene(nextSceneName);
+    }
+}
